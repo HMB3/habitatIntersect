8f1fd6a5daa5ccb114d508d31da802062fe9d0a7
Hugh_Burley <hugh.burley@gmail.com>
2021-03-27 10:55:58 +1100

    Update SDM_GEN_MODEL_LISTS.R
    - Add ALA object to data

--- R/SDM_GEN_MODEL_LISTS.R
+++ R/SDM_GEN_MODEL_LISTS.R
@@ -56,7 +56,6 @@ gbif_keep <- c(## TAXONOMY
 ## ALA Enviro cols ----
 ## Createa a table of all the ALA columns
 ALA_columns     <- ala_fields("occurrence_stored",as_is = TRUE)$description[231:395]
-# load("./data/ALA/Insects/Family/Cyclotornidae_ALA_records.RData")
 names(ALA)[58:215]
 
 
